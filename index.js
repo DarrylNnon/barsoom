72dc7d90743463b529faad643b6d3c48d023ccd2
Francklin Darryl <127830662+DarrylNnon@users.noreply.github.com>
2025-03-10 21:09:04 +0200

    initital file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as core from '@actions/core';
 
 const nameToGreet = core.getInput('name');
-const output_value = `Hello ${name} from Barsoom!`;
+const output_value = `Hello ${francklin} from Barsoom!`;
 core.setOutput('greeting', 'output_Value');
